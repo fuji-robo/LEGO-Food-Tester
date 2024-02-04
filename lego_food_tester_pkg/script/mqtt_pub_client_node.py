66fcf34f893d628f6a96cf99a226340f084d4520
Tomoaki Fujino <84122764+fuji-robo@users.noreply.github.com>
2024-02-04 21:50:22 +0900

    Update mqtt_pub_client_node.py

--- lego_food_tester_pkg/script/mqtt_pub_client_node.py
+++ lego_food_tester_pkg/script/mqtt_pub_client_node.py
@@ -14,10 +14,9 @@ class mqtt_pub_client:
         self.topic = "ev3_motor"
         self.timeout = 60
         
-        # MQTTの接続設定
-        self.client = mqtt.Client()                      # クラスのインスタンス(実体)の作成       
-        self.client.on_connect = self.on_connect         # 接続時のコールバック関数を登録
-        self.client.on_disconnect = self.on_disconnect   # 切断時のコールバックを登録
+        self.client = mqtt.Client()                         
+        self.client.on_connect = self.on_connect         
+        self.client.on_disconnect = self.on_disconnect   
         
         self.client.connect(self.broker_address, self.port, self.timeout)
         self.client.loop_start()
@@ -25,7 +24,7 @@ class mqtt_pub_client:
         self.publish(self.topic, 0, 0, 0, 0)
             
     def on_connect(self, client, userdata, flag, rc):
-        print("Connected with result code " + str(rc))  # 接続できた旨表示
+        print("Connected with result code " + str(rc))
     
     def on_disconnect(self, client, userdata, rc):
         if  rc != 0:
@@ -36,7 +35,7 @@ class mqtt_pub_client:
     
     def publish(self, topic : str, motor_a_speed : int, motor_a_rotation : float, motor_b_speed : int, motor_b_rotation : float):
         data_to_send = f"{str(motor_a_speed)},{'{:.2f}'.format(motor_a_rotation)},{str(motor_b_speed)},{'{:.2f}'.format(motor_b_rotation)}" # カンマで区切ったデータ
-        self.client.publish(topic, data_to_send)    # トピック名とメッセージを決めて送信
+        self.client.publish(topic, data_to_send)
         time.sleep(0.1)
 
 class listener:
